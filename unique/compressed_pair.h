c4edcd503cd547aa50a62ee6dc28bd616d0dc96b
Owltop <gg@g.kz>
2023-04-20 18:16:49 +0300

    unique

--- unique/compressed_pair.h
+++ unique/compressed_pair.h
@@ -1,3 +1,196 @@
 #pragma once
 
-// Paste here your implementation of compressed_pair from seminar 2 to use in UniquePtr
+#include <type_traits>
+#include <utility>
+
+template <typename F, typename S, bool = std::is_empty_v<F> && !std::is_final_v<F>,
+          bool = std::is_empty_v<S> && !std::is_final_v<S>>
+class CompressedPair;
+
+template <typename F, typename S>
+class CompressedPair<F, S, false, false> {
+
+public:
+    CompressedPair(){};
+
+    template <class U, class V>
+    CompressedPair(U&& first, V&& second)
+        : first_(std::forward<U>(first)), second_(std::forward<V>(second)) {
+    }
+
+    F& GetFirst() {
+        return first_;
+    }
+
+    const F& GetFirst() const {
+        return first_;
+    }
+
+    S& GetSecond() {
+        return second_;
+    };
+
+    const S& GetSecond() const {
+        return second_;
+    };
+
+private:
+    F first_{};
+    S second_{};
+};
+
+template <typename F, typename S>
+class CompressedPair<F, S, true, false> : F {
+
+public:
+    CompressedPair(){};
+
+    template <class U, class V>
+    CompressedPair(U&& first, V&& second)
+        : F(std::forward<U>(first)), second_(std::forward<V>(second)) {
+    }
+
+    F& GetFirst() {
+        return (*this);
+    }
+
+    const F& GetFirst() const {
+        return (*this);
+    }
+
+    S& GetSecond() {
+        return second_;
+    };
+
+    const S& GetSecond() const {
+        return second_;
+    };
+
+private:
+    S second_{};
+};
+
+template <typename F, typename S>
+class CompressedPair<F, S, false, true> : S {
+
+public:
+    CompressedPair(){};
+
+    template <class U, class V>
+    CompressedPair(U&& first, V&& second)
+        : first_(std::forward<U>(first)), S(std::forward<V>(second)) {
+    }
+
+    F& GetFirst() {
+        return first_;
+    }
+
+    const F& GetFirst() const {
+        return first_;
+    }
+
+    S& GetSecond() {
+        return (*this);
+    };
+
+    const S& GetSecond() const {
+        return (*this);
+    };
+
+private:
+    F first_{};
+};
+
+template <typename F, typename S>
+class CompressedPair<F, S, true, true> : F, S {
+
+public:
+    CompressedPair(){};
+
+    template <class U, class V>
+    CompressedPair(U&& first, V&& second) : F(std::forward<U>(first)), S(std::forward<V>(second)) {
+    }
+
+    F& GetFirst() {
+        return (*this);
+    }
+
+    const F& GetFirst() const {
+        return (*this);
+    }
+
+    S& GetSecond() {
+        return (*this);
+    };
+
+    const S& GetSecond() const {
+        return (*this);
+    };
+};
+
+template <typename F>
+class CompressedPair<F, F, true, true> : F {
+
+public:
+    CompressedPair(){};
+
+    // CompressedPair(const F& first, F&& second) : F(first) {};
+
+    template <typename U, typename V>
+    CompressedPair(U&& first, V&& second)
+        : F(std::forward<U>(first), first(std::forward<V>(second))){};
+
+    // CompressedPair(F&& first, const F& second)  : F(std::move(first)) {
+    // }
+
+    F& GetFirst() {
+        return (*this);
+    }
+
+    const F& GetFirst() const {
+        return (*this);
+    }
+
+    F& GetSecond() {
+        return (*this);
+    };
+
+    const F& GetSecond() const {
+        return (*this);
+    };
+
+private:
+    F first_{};
+};
+
+template <typename F>
+class CompressedPair<F, F, false, false> {
+
+public:
+    CompressedPair(){};
+
+    template <class U>
+    CompressedPair(U&& first, U&& second)
+        : first_(std::forward<U>(first)), second_(std::forward<U>(second)) {
+    }
+
+    F& GetFirst() {
+        return first_;
+    }
+
+    const F& GetFirst() const {
+        return first_;
+    }
+
+    F& GetSecond() {
+        return second_;
+    };
+
+    const F& GetSecond() const {
+        return second_;
+    };
+
+private:
+    F first_{};
+    F second_{};
+};
--- unique/unique.h
+++ unique/unique.h
@@ -3,54 +3,398 @@
 #include "compressed_pair.h"
 
 #include <cstddef>  // std::nullptr_t
+#include <exception>
 
 struct Slug {};
 
 // Primary template
+
 template <typename T, typename Deleter = Slug>
 class UniquePtr {
 public:
+    using Lref = std::add_lvalue_reference<T>;
     ////////////////////////////////////////////////////////////////////////////////////////////////
     // Constructors
 
-    explicit UniquePtr(T* ptr = nullptr);
-    UniquePtr(T* ptr, Deleter deleter);
+    explicit UniquePtr(T* ptr = nullptr) {
+        ptr_.GetFirst() = ptr;
+        Deleter del;
+        ptr_.GetSecond() = std::move(del);
+    }
 
-    UniquePtr(UniquePtr&& other);
+    UniquePtr(T* ptr, Deleter deleter) {
+        ptr_.GetFirst() = ptr;
+        ptr_.GetSecond() = std::move(deleter);
+    }
+
+    template <class U, class Del>
+    UniquePtr(UniquePtr<U, Del>&& other) noexcept {
+        ptr_.GetFirst() = other.ptr_.GetFirst();
+        ptr_.GetSecond() = std::move(other.ptr_.GetSecond());
+        other.ptr_.GetFirst() = nullptr;
+    }
+
+    UniquePtr(UniquePtr& other) = delete;
+    UniquePtr& operator=(UniquePtr& other) = delete;
 
     ////////////////////////////////////////////////////////////////////////////////////////////////
     // `operator=`-s
 
-    UniquePtr& operator=(UniquePtr&& other);
-    UniquePtr& operator=(std::nullptr_t);
+    template <class U, class Del>
+    UniquePtr& operator=(UniquePtr<U, Del>&& other) noexcept {
+        if (this->ptr_.GetFirst() == other.ptr_.GetFirst()) {
+            return *this;
+        }
+        if constexpr (std::is_same_v<Deleter, Slug>) {
+            delete ptr_.GetFirst();
+        } else {
+            Deleter()(ptr_.GetFirst());
+        }
+        ptr_.GetFirst() = other.ptr_.GetFirst();
+        ptr_.GetSecond() = std::move(other.ptr_.GetSecond());
+        other.ptr_.GetFirst() = nullptr;
+        return *this;
+    }
+
+    UniquePtr& operator=(std::nullptr_t) {
+        if constexpr (std::is_same_v<Deleter, Slug>) {
+            delete ptr_.GetFirst();
+        } else {
+            Deleter()(ptr_.GetFirst());
+        }
+        ptr_.GetFirst() = nullptr;
+        Deleter del;
+        ptr_.GetSecond() = std::move(del);
+        return *this;
+    }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////
     // Destructor
 
-    ~UniquePtr();
+    ~UniquePtr() {
+        if constexpr (std::is_same_v<Deleter, Slug>) {
+            delete ptr_.GetFirst();
+        } else {
+            Deleter()(ptr_.GetFirst());
+        }
+    };
 
     ////////////////////////////////////////////////////////////////////////////////////////////////
     // Modifiers
 
-    T* Release();
-    void Reset(T* ptr = nullptr);
-    void Swap(UniquePtr& other);
+    T* Release() {
+        auto old = ptr_.GetFirst();
+        ptr_.GetFirst() = nullptr;
+        return old;
+    };
+
+    void Reset(T* ptr = nullptr) {
+        auto old = ptr_.GetFirst();
+        ptr_.GetFirst() = ptr;
+        if constexpr (std::is_same_v<Deleter, Slug>) {
+            delete old;
+        } else {
+            Deleter()(old);
+        }
+    };
+
+    void Swap(UniquePtr& other) {
+        std::swap(ptr_.GetFirst(), other.ptr_.GetFirst());
+        std::swap(ptr_.GetSecond(), other.ptr_.GetSecond());
+    };
 
     ////////////////////////////////////////////////////////////////////////////////////////////////
     // Observers
 
-    T* Get() const;
-    Deleter& GetDeleter();
-    const Deleter& GetDeleter() const;
-    explicit operator bool() const;
+    T* Get() const {
+        return ptr_.GetFirst();
+    };
+    Deleter& GetDeleter() {
+        return ptr_.GetSecond();
+    };
+    const Deleter& GetDeleter() const {
+        return ptr_.GetSecond();
+    };
+    explicit operator bool() const {
+        return ptr_.GetFirst() != nullptr;
+    };
 
     ////////////////////////////////////////////////////////////////////////////////////////////////
     // Single-object dereference operators
 
-    T& operator*() const;
-    T* operator->() const;
+    std::add_lvalue_reference_t<T> operator*() const {
+        return *(ptr_.GetFirst());
+    };
+
+    T* operator->() const {
+        return ptr_.GetFirst();
+    };
+
+    // protected:
+    CompressedPair<T*, Deleter> ptr_;
 };
 
 // Specialization for arrays
 template <typename T, typename Deleter>
-class UniquePtr<T[], Deleter>;
+class UniquePtr<T[], Deleter> {
+public:
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    // Constructors
+
+    explicit UniquePtr(T* ptr = nullptr) {
+        ptr_.GetFirst() = ptr;
+        Deleter del;
+        ptr_.GetSecond() = std::move(del);
+    }
+
+    UniquePtr(T* ptr, Deleter deleter) {
+        ptr_.GetFirst() = ptr;
+        ptr_.GetSecond() = std::move(deleter);
+    }
+
+    template <class U, class Del>
+    UniquePtr(UniquePtr<U, Del>&& other) noexcept {
+        ptr_.GetFirst() = other.ptr_.GetFirst();
+        ptr_.GetSecond() = std::move(other.ptr_.GetSecond());
+        other.ptr_.GetFirst() = nullptr;
+    }
+
+    UniquePtr(UniquePtr& other) = delete;
+    UniquePtr& operator=(UniquePtr& other) = delete;
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    // `operator=`-s
+
+    template <class U, class Del>
+    UniquePtr& operator=(UniquePtr<U, Del>&& other) noexcept {
+        if (this->ptr_.GetFirst() == other.ptr_.GetFirst()) {
+            return *this;
+        }
+        if constexpr (std::is_same_v<Deleter, Slug>) {
+            delete[] ptr_.GetFirst();
+        } else {
+            Deleter()(ptr_.GetFirst());
+        }
+        ptr_.GetFirst() = other.ptr_.GetFirst();
+        ptr_.GetSecond() = std::move(other.ptr_.GetSecond());
+        other.ptr_.GetFirst() = nullptr;
+        return *this;
+    }
+
+    UniquePtr& operator=(std::nullptr_t) {
+        if constexpr (std::is_same_v<Deleter, Slug>) {
+            delete[] ptr_.GetFirst();
+        } else {
+            Deleter()(ptr_.GetFirst());
+        }
+        ptr_.GetFirst() = nullptr;
+        Deleter del;
+        ptr_.GetSecond() = std::move(del);
+        return *this;
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    // Destructor
+
+    ~UniquePtr() {
+        if constexpr (std::is_same_v<Deleter, Slug>) {
+            delete[] ptr_.GetFirst();
+        } else {
+            Deleter()(ptr_.GetFirst());
+        }
+    };
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    // Modifiers
+
+    T* Release() {
+        auto old = ptr_.GetFirst();
+        ptr_.GetFirst() = nullptr;
+        return old;
+    };
+
+    void Reset(T* ptr = nullptr) {
+        auto old = ptr_.GetFirst();
+        ptr_.GetFirst() = ptr;
+        if constexpr (std::is_same_v<Deleter, Slug>) {
+            delete[] old;
+        } else {
+            Deleter()(old);
+        }
+    };
+
+    void Swap(UniquePtr& other) {
+        std::swap(ptr_.GetFirst(), other.ptr_.GetFirst());
+        std::swap(ptr_.GetSecond(), other.ptr_.GetSecond());
+    };
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    // Observers
+
+    T* Get() const {
+        return ptr_.GetFirst();
+    };
+    Deleter& GetDeleter() {
+        return ptr_.GetSecond();
+    };
+    const Deleter& GetDeleter() const {
+        return ptr_.GetSecond();
+    };
+    explicit operator bool() const {
+        return ptr_.GetFirst() != nullptr;
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    // Single-object dereference operators
+
+    T& operator*() const {
+        return *(ptr_.GetFirst());
+    };
+    T* operator->() const {
+        return ptr_.GetFirst();
+    };
+
+    T& operator[](size_t i) const {
+        size_t cur = 0;
+        auto cur_p = ptr_.GetFirst();
+        while (cur != i) {
+            ++cur_p;
+            ++cur;
+        }
+        return *cur_p;
+    }
+
+    T& operator[](size_t i) {
+        size_t cur = 0;
+        auto cur_p = ptr_.GetFirst();
+        while (cur != i) {
+            ++cur_p;
+            ++cur;
+        }
+        return *cur_p;
+    }
+
+    // protected:
+    CompressedPair<T*, Deleter> ptr_;
+};
+
+template <typename Deleter>
+class UniquePtr<void, Deleter> {
+public:
+    using Lref = std::add_lvalue_reference<void>;
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    // Constructors
+
+    explicit UniquePtr(void* ptr = nullptr) {
+        ptr_.GetFirst() = ptr;
+        Deleter del;
+        ptr_.GetSecond() = std::move(del);
+    }
+
+    UniquePtr(void* ptr, Deleter deleter) {
+        ptr_.GetFirst() = ptr;
+        ptr_.GetSecond() = std::move(deleter);
+    }
+
+    template <class U, class Del>
+    UniquePtr(UniquePtr<U, Del>&& other) noexcept {
+        ptr_.GetFirst() = other.ptr_.GetFirst();
+        ptr_.GetSecond() = std::move(other.ptr_.GetSecond());
+        other.ptr_.GetFirst() = nullptr;
+    }
+
+    UniquePtr(UniquePtr& other) = delete;
+    UniquePtr& operator=(UniquePtr& other) = delete;
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    // `operator=`-s
+
+    template <class U, class Del>
+    UniquePtr& operator=(UniquePtr<U, Del>&& other) noexcept {
+        if (this->ptr_.GetFirst() == other.ptr_.GetFirst()) {
+            return *this;
+        }
+        if constexpr (std::is_same_v<Deleter, Slug>) {
+            delete ptr_.GetFirst();
+        } else {
+            Deleter()(ptr_.GetFirst());
+        }
+        ptr_.GetFirst() = other.ptr_.GetFirst();
+        ptr_.GetSecond() = std::move(other.ptr_.GetSecond());
+        other.ptr_.GetFirst() = nullptr;
+        return *this;
+    }
+
+    UniquePtr& operator=(std::nullptr_t) {
+        if constexpr (std::is_same_v<Deleter, Slug>) {
+            delete ptr_.GetFirst();
+        } else {
+            Deleter()(ptr_.GetFirst());
+        }
+        ptr_.GetFirst() = nullptr;
+        Deleter del;
+        ptr_.GetSecond() = std::move(del);
+        return *this;
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    // Destructor
+
+    ~UniquePtr() {
+        if constexpr (std::is_same_v<Deleter, Slug>) {
+            delete ptr_.GetFirst();
+        } else {
+            Deleter()(ptr_.GetFirst());
+        }
+    };
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    // Modifiers
+
+    void* Release() {
+        auto old = ptr_.GetFirst();
+        ptr_.GetFirst() = nullptr;
+        return old;
+    };
+
+    void Reset(void* ptr = nullptr) {
+        auto old = ptr_.GetFirst();
+        ptr_.GetFirst() = ptr;
+        if constexpr (std::is_same_v<Deleter, Slug>) {
+            delete old;
+        } else {
+            Deleter()(old);
+        }
+    };
+
+    void Swap(UniquePtr& other) {
+        std::swap(ptr_.GetFirst(), other.ptr_.GetFirst());
+        std::swap(ptr_.GetSecond(), other.ptr_.GetSecond());
+    };
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    // Observers
+
+    void* Get() const {
+        return ptr_.GetFirst();
+    };
+    Deleter& GetDeleter() {
+        return ptr_.GetSecond();
+    };
+    const Deleter& GetDeleter() const {
+        return ptr_.GetSecond();
+    };
+    explicit operator bool() const {
+        return ptr_.GetFirst() != nullptr;
+    };
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    // Single-object dereference operators
+
+    void* operator->() const {
+        return ptr_.GetFirst();
+    };
+
+    // protected:
+    CompressedPair<void*, Deleter> ptr_;
+};
